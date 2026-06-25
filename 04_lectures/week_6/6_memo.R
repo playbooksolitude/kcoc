6c629640e0efe562cd9bc9ab4145614292b26276
playbooksolitude <slatopr@gmail.com>
2026-06-25 22:43:37 +0900

    .

--- 04_lectures/week_6/6_memo.R
+++ 04_lectures/week_6/6_memo.R
@@ -1,31 +1,6 @@
 library(leaflet)
 library(tidygeocoder)
 
-
-##
-author: "최요한"
-date: "`r Sys.Date()`"
-format: 
-  html:
-  toc: true
-toc-depth: 2           # 번호 깊이
-toc-float: true        # 스크롤을 내려도 목차가 따라옵니다.
-number-depth: 3
-number-sections: true   # 자동 번호
-code-fold: false         # 
-code-summary: "소스 코드 확인하기"
-smooth-scroll: true  # 클릭 시 부드럽게 이동
-code-copy: true        # 코드 복사 버튼 활성화
-# theme: Zephyr
-# theme: Flatly
-#theme:
-#  light: [flatly, cosmo]  #본문 flatly, #내비게이션 등 cosmo 적용
-#  dark: darkly
-# css: styles.css
-embed-resources: true
-editor: visual
-
-
 ##
 
 leaflet() |> 
@@ -33,7 +8,13 @@ leaflet() |>
   addTiles() |> 
   addMarkers(174.764, -36.877, "Maungawhau")
 
-library(leaflet)
+#우간다
+leaflet() |>
+  setView(32.5825, 0.3476, zoom = 13) |>
+  addTiles() |>
+  addMarkers(32.5825, 0.3476, "Kampala")
+
+
 
 # 서울시청 좌표: 경도(126.9784), 위도(37.5665)
 leaflet() |> 
@@ -51,15 +32,18 @@ leaflet() |>
 
 # 주소로 찾ㅣ
 df_addresses <- data.frame(
-  name = c("서울시청", "부산역", "제주 성산일출봉"),
-  address = c("서울특별시 중구 세종대로 110", 
+  name = c("KCOC", "부산역", "제주 성산일출봉"),
+  address = c("서울시 마포구 독막로 282", 
               "부산광역시 동구 중앙대로 206", 
               "제주특별자치도 서귀포시 성산읍 성산리 1")
 )
 
+
+
 #
 # 2. 지오코딩 수행 (오픈스트리트맵 기반 'osm' 서비스 이용)
 df_coords <- df_addresses |>
+#df_coords <- kcoc_addresses |>
   geocode(address = address, 
           method = 'osm', 
           lat = latitude, 
@@ -70,6 +54,88 @@ print(df_coords)
 
 # 4. 변환된 좌표로 바로 leaflet 지도 그리기
 leaflet(df_coords) |>
-  setView(lng = 127.5, lat = 36.5, zoom = 7) |> # 대한민국 중심부
+  setView(lng = 127, lat = 37.5, zoom = 18) |> # 대한민국 중심부
   addTiles() |>
   addMarkers(lng = ~longitude, lat = ~latitude, popup = ~name)
+
+
+kcoc_addresses <- tibble(
+  name = "kcoc",
+  address = "서울시 마포구 독막로 282"
+)
+
+
+# 주소로 찾기
+df_addresses <- data.frame(
+  name = c("서울시청", "부산역", "제주 성산일출봉"),
+  address = c("서울특별시 중구 세종대로 110", 
+              "부산광역시 동구 중앙대로 206", 
+              "제주특별자치도 서귀포시 성산읍 성산리 1")
+)
+
+#
+# 2. 지오코딩 수행 (오픈스트리트맵 기반 'osm' 서비스 이용)
+#df_coords <- df_addresses |>
+df_coords <- kcoc_addresses |>
+  geocode(address = address, 
+          method = 'osm', 
+          lat = latitude, 
+          long = longitude)
+
+#
+kcoc_addresses <- tibble(
+  name = "kcoc",
+  address = "서울시 마포구 독막로 282"
+)
+
+df_coords <- kcoc_addresses |>
+  geocode(address = address, 
+          method = 'osm', 
+          lat = latitude, 
+          long = longitude)
+
+print(df_coords)
+
+leaflet(df_coords) |>
+  setView(lng = 127, lat = 37.5, zoom = 14) |> # 대한민국 중심부
+  addTiles() |>
+  addMarkers(lng = ~longitude, lat = ~latitude, popup = ~name)
+
+#
+library(tidygeocoder)
+library(leaflet)
+library(dplyr)
+
+kcoc_addresses <- tibble(
+  name = "KCOC",
+  address = "서울시 마포구 독막로 282"
+)
+
+df_coords <- kcoc_addresses |>
+  geocode(
+    address = address,
+    method = "osm",
+    lat = latitude,
+    long = longitude
+  )
+
+
+# 주소로 찾ㅣ
+df_addresses <- data.frame(
+  name = c("KCOC"),
+  address = c("서울시 마포구 독막로 282")
+)
+
+df_addresses |>
+  geocode(
+    address = address,
+    method = "osm",
+    lat = latitude,
+    long = longitude
+  )
+
+leaflet(df_addresses) |>
+  setView(lng = 127, lat = 37.5, zoom = 14) |> # 대한민국 중심부
+  addTiles() |>
+  addMarkers(lng = ~longitude, lat = ~latitude, popup = ~name)
+
